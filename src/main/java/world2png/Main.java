ee9d602c5165c0fcfec16fab83688ea5d975ffe0
Hancho1577 <vojougae35@gmail.com>
2020-03-31 22:56:36 +0700

    for high quality

--- src/main/java/world2png/Main.java
+++ src/main/java/world2png/Main.java
@@ -13,41 +13,51 @@ import java.io.IOException;
 
 public class Main extends PluginBase {
 
-    private int X;
-    private int Y;
-    private String world;
+	private int X;
+	private int Y;
+	private int Size;
 
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
-        X = getConfig().getInt("x_size");
-        Y = getConfig().getInt("y_size");
-        world = getConfig().getString("world");
-        getServer().getScheduler().scheduleAsyncTask(this, new AsyncTask() {
-            @Override
-            public void onRun() {
-                getLogger().info("\u00A7aGenerating picture of world \"" + world + "\"...");
-                BufferedImage image = new BufferedImage(X, Y, BufferedImage.TYPE_INT_RGB);
-                Graphics2D graphics = image.createGraphics();
-                Level level = getServer().getLevelByName(world);
-                if (level == null) {
-                    getLogger().error("\u00A7cWorld cannot be null!");
-                    return;
-                }
-                Position spawn = level.getSpawnLocation();
-                for (int x = 0; x != X; x++) {
-                    for (int y = 0; y != Y; y++) {
-                        graphics.setColor(new Color(level.getMapColorAt(spawn.getFloorX() - (X >> 1) + x, spawn.getFloorZ() - (Y >> 1) + y).getRGB()));
-                        graphics.fillRect(x, y, x, y);
-                    }
-                    getLogger().info("\u00A7a" + (x * 100 / X) + "% done");
-                }
-                try {
-                    File file = new File(getDataFolder() + File.separator + world + ".png");
-                    ImageIO.write(image, "png", file);
-                    getLogger().info("\u00A7a" + getDataFolder() + File.separator + world + ".png" + " saved!");
-                } catch (IOException ignored) {}
-            }
-        });
-    }
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		X = getConfig().getInt("x_size");
+		Y = getConfig().getInt("y_size");
+		Size = getConfig().getInt("size");
+		getServer().getScheduler().scheduleAsyncTask(this, new AsyncTask() {
+			@Override
+			public void onRun() {
+				getLogger().info("\u00A7aGenerating world.png...");
+
+				BufferedImage image;
+				Position spawn = getServer().getDefaultLevel().getSpawnLocation();
+				Level level = getServer().getDefaultLevel();
+				int rX = 0;
+				int rY = 0;
+
+				image = new BufferedImage(X * Size, Y * Size, BufferedImage.TYPE_INT_RGB);
+				Graphics2D graphics = image.createGraphics();
+
+				for (int x = 0; x != X; x++) {
+					for (int y = 0; y != Y; y++) {
+						graphics.setColor(new Color(
+								level.getMapColorAt(spawn.getFloorX() - (X / 2) + x, spawn.getFloorZ() - (Y / 2) + y)
+										.getRGB()));
+
+						graphics.fillRect(rX, rY, Size, Size);
+						rY += Size;
+					}
+					rY = 0;
+					rX += Size;
+					if ((x * 100 / X) % 5 == 0)
+						getLogger().info("\u00A7a" + (x * 100 / X) + "% done");
+				}
+				try {
+					File file = new File(getDataFolder() + "/world.png");
+					ImageIO.write(image, "png", file);
+					getLogger().info("\u00A7aworld.png saved!");
+				} catch (IOException e) {
+				}
+			}
+		});
+	}
 }
